2da82fe7ce1b6938b0169627d6f266c41895068c
rudrakumar2012 <manaw10071994@gmail.com>
2024-02-06 22:48:27 +0530

    Login and Connected created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import abi from "./contractJson/Voting.json"
+import { ethers } from 'ethers';
+import Login from "./Components/Login";
+import Connected from "./Components/Connected"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const VotingContractAddress = "0xA860b5865FB1cDA9bbc956dbF49F8ffac6D34C2D";
+  const VotingAbi = abi.abi;
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+    }
+
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener(
+          "accountsChanged",
+          handleAccountsChanged
+        );
+      }
+    };
+  },[provider]);
+
+  function handleAccountsChanged(accounts) {
+    if (accounts.length > 0 && account !== accounts[0]) {
+      setAccount(accounts[0]);
+    } else {
+      setIsConnected(false);
+      setAccount(null);
+    }
+  }
+
+  async function connectToMetamask() {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        console.log("Metamask Connected: " + address);
+        setIsConnected(true);
+      } catch (error) {
+        alert(error);
+      }
+    } else {
+      alert("Metamask is not detected in the browser");
+    }
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-gradient-to-br from-purple-400 to-pink-500 flex flex-col justify-center items-center">
+      {isConnected ? (
+        <Connected account={account}/>
+      ) : (
+        <Login connectWallet={connectToMetamask} />
+      )}
+    </div>
   )
 }
 
